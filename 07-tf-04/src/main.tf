a95556c62588642b475800360907b60cd00a3a5f
Timur Khakimov <timurkin@mail.ru>
2023-06-03 01:18:26 +0500

    [07-tf-04] Homework

---
+++ 07-tf-04/src/main.tf
@@ -0,0 +1,44 @@
+module "vpc_prod" {
+  source   = "./vpc"
+  env_name = "production"
+  vpc_name = "netology"
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
+  ]
+}
+
+module "vpc_dev" {
+  source   = "./vpc"
+  env_name = "develop"
+  vpc_name = "netology"
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+  ]
+}
+
+module "test-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = var.default_env
+  network_id     = module.vpc_dev.vpc_id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = module.vpc_dev.subnets_ids
+  instance_name  = "web"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
+
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars = {
+    "ssh_key" = file("~/.ssh/id_ed25519.pub")
+  }
+}
+
